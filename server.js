d74c0c3fa88665e84a63ecde685a3314d3cb8adf
Jenna's Bootcamp <work@Jennas-MacBook-Pro.local>
2018-02-23 16:17:38 -0600

    adds stuff

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var path = require("path");
-var app = require("express");
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-var PORT = procces.env.PORT || 3000;
-
+app.use(express.static('app/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+})
